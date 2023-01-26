2d12f4fbec84463789eb4b9801d549c2c039c918
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-26 16:08:57 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -13,7 +13,7 @@ import random
 @channelforward.on_message(filters.command("start") & filters.private & filters.incoming)
 async def start(client, message):
     await message.reply_photo(
-            text = Translation.START.format(
+            text = START.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
                 username = None if not message.from_user.username else '@' + message.from_user.username,

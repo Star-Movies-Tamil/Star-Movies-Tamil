4b2e34b9b875bd1d4fab1e5eb4b5b2e6142e74a1
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-27 20:18:33 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -40,8 +40,7 @@ def callback_query(Client, CallbackQuery):
         InlineKeyboardButton('😎 About', callback_data="about")
     ]
 ]
-        CallbackQuery.edit_message_text(
-            text=reply_text,
+        CallbackQuery.edit_message_text(text=reply_text,
             HELP,
             reply_markup = InlineKeyboardMarkup(HELP_BUTTON)
         )

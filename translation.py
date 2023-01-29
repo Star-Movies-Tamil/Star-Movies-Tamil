b4402bb79f0fe67fa1b8ab496128401bd05a10af
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-29 15:47:46 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,7 +1,7 @@
 class Translation(object):
 
       # Start Text
-      START = """<b>Hi 👋🏻 {mention},
+      START = """<b>Hi 👋🏻 {},
 
 I'm UK Studios Official a Bot to Maintain Your Channels. I am very useful for the Channel Admin who have many Channels.
 
@@ -29,7 +29,7 @@ Maintained By : [Karthik](https://t.me/HMTD_Karthik)</b>"""
 🌐 Website : [www.HMTDMovies.tk](https://www.HMTDMovies.tk)</b>"""
 
       #Support Text
-      SUPPORT = """Hi {mention},"""
+      SUPPORT = """Hi {},"""
 
       # Alien Covenant Movie
       ALIEN_COVENANT = """<b> Hi {mention},

f480b96577cf54302201c431d92a317445bc5327
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-30 18:55:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,7 @@ from os import getenv
 class Config(object):
       API_HASH = getenv("API_HASH", "5264bf4663e9159565603522f58d3c18")
       API_ID = int(getenv("API_ID", 11973721))
-      AS_COPY = True if getenv("AS_COPY", True) == "True" else False
       BOT_TOKEN = getenv("BOT_TOKEN", "5949999646:AAGNAzsUTMutsqtnSk2R4MkGgCQ0uMtqbIU")
-      CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001861300920:-1001811940117 -1001786688631 -1001436081117 -1001780758150 -1001721348234 -1001780697340 -1001698854544 -1001593574364 -1001519485548").replace("\n", " ").split(' '))
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://umeshnamo:8907711426@cluster0.mbtdc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 ADMINS = int(os.environ.get("ADMINS", "1391556668"))

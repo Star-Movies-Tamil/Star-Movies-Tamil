194ee7850d62075a3a8b3aaf2b15c2575e13e235
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-31 03:41:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ class Config(object):
 DB_URI = os.environ.get("DATABASE_URL", "postgres://udpqqmbqqrkpdq:5e05cd23ce9587cb94844272c62111f2e3fc21d748ae3763abbf24de4ede6693@ec2-35-169-184-61.compute-1.amazonaws.com:5432/d37uqkso189vgg")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 ADMINS = int(os.environ.get("ADMINS", "1391556668"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001589399161"))

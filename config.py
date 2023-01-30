d34e557479ddce09839cdcc3c4cc3e4c94b8c7d1
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-30 21:11:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
       API_HASH = getenv("API_HASH", "5264bf4663e9159565603522f58d3c18")
       API_ID = int(getenv("API_ID", 11973721))
       BOT_TOKEN = getenv("BOT_TOKEN", "5949999646:AAGNAzsUTMutsqtnSk2R4MkGgCQ0uMtqbIU")
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://umeshnamo:8907711426@cluster0.mbtdc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://udpqqmbqqrkpdq:5e05cd23ce9587cb94844272c62111f2e3fc21d748ae3763abbf24de4ede6693@ec2-35-169-184-61.compute-1.amazonaws.com:5432/d37uqkso189vgg")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 ADMINS = int(os.environ.get("ADMINS", "1391556668"))

a5f75fbb29abfa5e6a549d58766ed1a073acd8cc
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-30 06:21:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+import logging
 from os import getenv
 
 class Config(object):
@@ -6,3 +8,5 @@ class Config(object):
       AS_COPY = True if getenv("AS_COPY", True) == "True" else False
       BOT_TOKEN = getenv("BOT_TOKEN", "5949999646:AAGNAzsUTMutsqtnSk2R4MkGgCQ0uMtqbIU")
       CHANNEL = list(x for x in getenv("CHANNEL_ID", "-1001861300920:-1001811940117 -1001786688631 -1001436081117 -1001780758150 -1001721348234 -1001780697340 -1001698854544 -1001593574364 -1001519485548").replace("\n", " ").split(' '))
+      DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://KarthikMovies:KarthikUK007@cluster0.4l5byki.mongodb.net/?retryWrites=true&w=majority")
+      DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")

a166b1a152a428ecfe25fee77a8ded3bcb6546a3
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-01-31 04:38:06 +0530

    Update broadcast.py

--- Plugins/broadcast.py
+++ Plugins/broadcast.py
@@ -1,8 +1,14 @@
-from database.database import db
+from pyrogram import Client, filters
+import datetime
+import time
+from database.users_chats_db import db
+from info import ADMINS
+from utils import broadcast_messages, broadcast_messages_group
+import asyncio
 
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 # https://t.me/GetTGLink/4178
-async def verupikkals(bot, message):
+async def verupikkals(client, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(

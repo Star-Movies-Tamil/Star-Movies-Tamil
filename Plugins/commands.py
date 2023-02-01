885a5c3fbba387353d95ba6a9383bef554523c56
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-01 20:31:40 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -255,11 +255,45 @@ async def get_users(client, message):
 async def broadcast_handler_open(_, m):
     if m.from_user.id not in AUTH_USERS:
         return
+        query = await full_userbase()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+        
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        for chat_id in query:
+            try:
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except UserIsBlocked:
+                await del_user(chat_id)
+                blocked += 1
+            except InputUserDeactivated:
+                await del_user(chat_id)
+                deleted += 1
+            except:
+                unsuccessful += 1
+                pass
+            total += 1
+        
+        status = f"""<b><u>Broadcast Completed</u>
+Total Users: <code>{total}</code>
+Successful: <code>{successful}</code>
+Blocked Users: <code>{blocked}</code>
+Deleted Accounts: <code>{deleted}</code>
+Unsuccessful: <code>{unsuccessful}</code></b>"""
     if m.reply_to_message is None:
         await m.delete()
     else:
         await broadcast(m, db)
-        msg = await m.reply(REPLY_ERROR)
+        msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
 
 ################################################################################################################################################################################################################################################

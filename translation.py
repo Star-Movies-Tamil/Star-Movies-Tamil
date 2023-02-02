bbbfa78cd12442b2eba8fb349cfaf7dcf1facef7
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-02 07:15:46 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -38,31 +38,3 @@ Movie Commends
 
       #Support Text
       SUPPORT = """<b>I'm Star Movies Official Feedback Bot. You Want Any Help or Of You Need Any Movie Links Massage us Me and Get Solution to Admins</b>"""
-
-      # Star Movies Tamil
-
-      # Alien Covenant Movie
-      ALIEN_COVENANT = """<b>Hi 👋🏻 {mention} ♥️,
-
-Alien Covenant (2017) (BluRay + Original Audios) - [1080p & 720p - x264 - [Tamil + Telugu + Hindi + English] - (DD5.1 - 448Kbps) - 3.1GB & 1.5GB - 1.4GB | x264 - [Tamil + Telugu + Hindi] - 450MB] - ESub]
-
-Click here to Download 👇
-
-✅ Telegram Files 📂
-
-450MB :- [Click here](https://link.tnlink.in/J7B0Zd)
-
-450MB :- [Click here](https://link.tnlink.in/gRLN8)
-
-1.4GB (720p) :- [Click here](https://link.tnlink.in/ynxnKCUR)
-
-1.5GB (720p) :- [Click here](https://link.tnlink.in/rIFE7x2)
-
-3GB (1080p) :- [Click here](https://link.tnlink.in/t9QN)
-
-3.1GB (1080p) :- [Click here](https://link.tnlink.in/LmLw)
-
-❗Note :- We're Recommend Brave Browser
-
-📢 Updates Channel :- [Star Movies Tamil](https://t.me/Star_Moviess_Tamil)</b>"""
-

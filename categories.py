96ca5490e8c0a3a64fbbe6e4a02ab544c784c2ee
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-03 20:47:42 +0530

    Update categories.py

--- categories.py
+++ categories.py
@@ -3,4 +3,33 @@ class Categories(object):
       # Tamil Movies
       TAMIL_MOVIES = """<b>Hi 👋🏻 {mention} ♥️,
 
-Alien Covenant (2017) :- /alien_covenant</b>"""
+You Can Get Tamil Movies 🎥 with Commends and Our Bot will Support Upto 4GB Files 📂.</b>"""
+
+
+      # Hollywood Movies
+      HOLLYWOOD_MOVIES = """<b>Hi 👋🏻 {mention} ♥️,
+
+You Can Get Hollywood Movies 🎥 with Commends and Our Bot will Support Upto 4GB Files 📂.
+
+Movies List
+♦️ Alien Covenant (2017) :- /alien_covenant</b>"""
+
+      # Web Series
+      WEB_SERIES = """<b>Hi 👋🏻 {mention} ♥️,
+
+You Can Get Web Series 🎥 with Commends and Our Bot will Support Upto 4GB Files 📂.</b>"""
+
+      # Movie Collection
+      MOVIE_COLLECTION = """<b>Hi 👋🏻 {mention} ♥️,
+
+You Can Get Movie Collection 🎥 with Commends and Our Bot will Support Upto 4GB Files 📂.</b>"""
+
+      # Cartoon Movies
+      CARTOON_MOVIES = """<b>Hi 👋🏻 {mention} ♥️,
+
+You Can Get Cartoon Movies 🎥 with Commends and Our Bot will Support Upto 4GB Files 📂.</b>"""
+
+      # TV Shows
+      TV_SHOWS = """<b>Hi 👋🏻 {mention} ♥️,
+
+You Can Get TV Shows 🎥 with Commends and Our Bot will Support Upto 4GB Files 📂.</b>"""

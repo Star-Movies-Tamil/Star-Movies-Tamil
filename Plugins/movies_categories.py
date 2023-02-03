54d5cbf652306602f4ca1dfd922a507e99c4ac42
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-03 19:46:39 +0530

    Update movies_categories.py

--- Plugins/movies_categories.py
+++ Plugins/movies_categories.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters
 from bot import Star_Moviess_Tamil
 from config import Config
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
-from movies import Movies
+from categories import Categories
 from pyrogram.errors import MessageNotModified, UserIsBlocked, InputUserDeactivated, FloodWait
 import random
 import os
@@ -26,7 +26,7 @@ import config
 @Star_Moviess_Tamil.on_message(filters.command("tamil_movies") & filters.private & filters.incoming)
 async def tamil_movies(client, message):
     await message.reply(
-        text = Movies.TAMIL_MOVIES.format(
+        text = Categories.TAMIL_MOVIES.format(
                 mention = message.from_user.mention
             ),
         disable_web_page_preview=True,

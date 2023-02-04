492a178d91499faf6378a1bedffbc91408b9c9fd
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-04 08:50:41 +0530

    Update movie_poster.py

--- Plugins/movie_poster.py
+++ Plugins/movie_poster.py
@@ -35,3 +35,31 @@ async def alien_covenant(client, message):
     )
 
 ################################################################################################################################################################################################################################################
+# Thunivu (2023)
+
+@Star_Moviess_Tamil.on_message(filters.command("thunivu") & filters.private & filters.incoming)
+async def thunivu(client, message):
+    await message.reply_photo(
+        caption = Poster.THUNIVU.format(
+                mention = message.from_user.mention
+            ),
+        photo="https://telegra.ph/file/1919c316d8d742d1fa3ab.jpg",
+        quote=True,
+    )
+
+################################################################################################################################################################################################################################################
+# Varisu (2023)
+
+@Star_Moviess_Tamil.on_message(filters.command("varisu") & filters.private & filters.incoming)
+async def varisu(client, message):
+    await message.reply_photo(
+        caption = Poster.VARISU.format(
+                mention = message.from_user.mention
+            ),
+        photo="https://telegra.ph/file/76ba5435a2983a6c42c80.jpg",
+        quote=True,
+    )
+
+################################################################################################################################################################################################################################################
+
+

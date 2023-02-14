354447f22898aea7c8015ee81a9e524f4981b6fa
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-14 21:10:41 +0530

    Update useless.py

--- Plugins/useless.py
+++ Plugins/useless.py
@@ -2,7 +2,7 @@ import base64
 import re
 import asyncio
 from pyrogram import filters
-from config import FORCE_SUB_CHANNEL, ADMINS
+from config import ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 

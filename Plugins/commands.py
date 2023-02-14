f47efedce27148b6143a793c576b71e74b8ffa5b
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-14 20:36:28 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -3,10 +3,9 @@ logger = logging.getLogger(__name__)
 
 from pyrogram import Client, filters, enums
 from bot import Star_Moviess_Tamil
-from config import ADMINS, AUTH_USERS, FILE_STORE_CHANNEL
+from config import ADMINS, AUTH_USERS
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, CallbackQuery
 from translation import Translation
-from handlers.file_store import unpack_new_file_id
 from pyrogram.errors import MessageNotModified, UserIsBlocked, InputUserDeactivated, FloodWait
 import random
 import os
@@ -22,13 +21,6 @@ import config
 from handlers.broadcast import broadcast
 from handlers.check_user import handle_user_status
 from handlers.database import Database
-
-async def allowed(_, __, message):
-    if FILE_STORE_CHANNEL:
-        return True
-    if message.from_user and message.from_user.id in ADMINS:
-        return True
-    return False
 
 LOG_CHANNEL = config.LOG_CHANNEL
 AUTH_USERS = config.AUTH_USERS
@@ -448,20 +440,23 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
 ################################################################################################################################################################################################################################################
 # Reply to a Message or a File 📂. I Will Give You a Sharable Permanent Link
 
-@Star_Moviess_Tamil.on_message(filters.command('link') & filters.create(allowed))
-async def gen_link_s(client, message):
-    replied = message.reply_to_message
-    if not replied:
-        return await message.reply('**Reply to a Message or a File. I Will Give You a Sharable Permanent Link**')
-    file_type = replied.media
-    if file_type not in [enums.MessageMediaType.VIDEO, enums.MessageMediaType.AUDIO, enums.MessageMediaType.DOCUMENT]:
-        return await message.reply("**Reply to a Support Media")
-    if message.has_protected_content and message.chat.id not in ADMINS:
-        return await message.reply("**Ok Bro**")
-    file_id, ref = unpack_new_file_id((getattr(replied, file_type.value)).file_id)
-    string = 'filep_' if message.text.lower().strip() == "/link" else 'file_'
-    string += file_id
-    outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    await message.reply(f"<b>⪼ Here is Your File Link :\nhttps://telegram.me/{temp.U_NAME}?start={outstr}</b>")
-    
+@Star_Moviess_Tamil.on_message(filters.private & filters.user(ADMINS) & filters.command('link'))
+async def link_generator(client: Client, message: Message):
+    while True:
+        try:
+            channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:
+            return
+        msg_id = await get_message_id(client, channel_message)
+        if msg_id:
+            break
+        else:
+            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
+            continue
+
+    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
+    link = f"https://t.me/{client.username}?start={base64_string}"
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+
 ################################################################################################################################################################################################################################################

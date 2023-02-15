f2a5be806e1def455e98676ddf0f877331dbd1c9
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-02-15 16:28:30 +0300

    Create start_filter.py

---
+++ Plugins/start_filter.py
@@ -0,0 +1,9 @@
+from pyrogram import filters
+
+def s_filter(_,__,message):
+   if str(message.text) == "start":
+      return True
+   else:
+      return False
+
+start_filter = filters.create(s_filter)

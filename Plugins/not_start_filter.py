a8b74bf26bdc81214f0e1925c9147d87e5becc5c
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-02-15 16:57:26 +0300

    Update and rename not_start_filter.py to varisu.py

--- Plugins/not_start_filter.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import filters
-
-def ns_filter(_,__,message):
-   if str(message.text)[:4] == "start":
-      return True
-   else:
-      return False
-
-not_start_filter = filters.create(ns_filter)
---
+++ Plugins/varisu.py
@@ -0,0 +1,12 @@
+from pyrogram import filters
+
+def ns_filter(_,__,message):
+   if str(message.text)[1:6] == "start":
+     if str(message.text)[6:] == "varisu":
+       return True
+     else:
+       return False
+   else:
+      return False
+
+not_start_filter = filters.create(ns_filter)

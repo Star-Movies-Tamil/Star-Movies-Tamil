da33f0573f74610d5bcbdbc23471123c1ea6c572
HMTD-Links <109021412+HMTD-Links@users.noreply.github.com>
2023-02-15 18:23:19 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -438,25 +438,4 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         await cb.message.delete(True)
 
 ################################################################################################################################################################################################################################################
-# Reply to a Message or a File 📂. I Will Give You a Sharable Permanent Link
 
-@Star_Moviess_Tamil.on_message(filters.private & filters.user(ADMINS) & filters.command('link'))
-async def link_generator(client: Client, message: Message):
-    while True:
-        try:
-            channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
-            return
-        msg_id = await get_message_id(client, channel_message)
-        if msg_id:
-            break
-        else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
-            continue
-
-    base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
-
-################################################################################################################################################################################################################################################

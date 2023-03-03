7b6fd6f75274a7473121fb54ac51a7cfdfce4ae8
N.Y <88596711+NYOFFICIALGH@users.noreply.github.com>
2023-03-03 18:40:30 +0300

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -345,7 +345,7 @@ async def send_msg(c, m: Message):
     await m.reply("**Give the User ID you Want me to Send Message.**",
         quote=True
     )
-    user_id = await listen_message(c,m.chat.id,timeout=None)
+    user_id = await listen_message(Client,m.chat.id,timeout=None)
     await c.send_message(int(user_id) , msg )
     await m.reply("**Messsage Sent.**",
         quote=True
